fb1c25f76c2c2cefb7f254410979e70f339d0144
Valeriia <vvoshta@gmail.com>
2024-03-21 16:39:14 +0300

    update Uploader

--- src/components/Uploader/Uploader.tsx
+++ src/components/Uploader/Uploader.tsx
@@ -1,28 +1,16 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+import { PlusOutlined } from '@ant-design/icons';
 import { Upload } from 'antd';
-import type { GetProp, UploadProps } from 'antd';
-
-import { setAvatar } from '../../store/reducers/PersonalInfoSlice';
+import type { UploadProps } from 'antd';
 
 import * as S from './style';
 
-type FileType = Parameters<GetProp<UploadProps, 'beforeUpload'>>[0];
-
-const getBase64 = (img: FileType): Promise<string> =>
+const getBase64 = (file: File): Promise<string> =>
     new Promise((resolve, reject) => {
-        console.log('Starting conversion to base64...');
         const reader = new FileReader();
-        reader.readAsDataURL(img);
-        reader.onload = () => {
-            console.log('Base64 conversion successful');
-            resolve(reader.result as string);
-        };
-        reader.onerror = (error) => {
-            console.error('Error during base64 conversion:', error);
-            reject(error);
-        };
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result as string);
+        reader.onerror = (error) => reject(error);
     });
 
 type UploaderProps = {
@@ -30,34 +18,19 @@ type UploaderProps = {
 };
 
 export const Uploader: React.FC<UploaderProps> = ({ onBase64Change }) => {
-    const [loading, setLoading] = useState(false);
     const [imageUrl, setImageUrl] = useState<string>();
-    const dispatch = useDispatch();
 
-    const handleChange: UploadProps['onChange'] = async (info) => {
-        console.log(info.file.status);
-
-        if (info.file.status === 'uploading') {
-            setLoading(true);
-            return;
-        }
-
-        if (info.file.originFileObj) {
-            try {
-                const base64String = await getBase64(info.file.originFileObj);
-                setLoading(false);
-                setImageUrl(base64String);
-                dispatch(setAvatar(base64String));
-                onBase64Change(base64String);
-            } catch (error) {
-                console.error('Error while converting to base64:', error);
-            }
+    const handleChange: UploadProps['onChange'] = async (fileList) => {
+        if (fileList.file && fileList.file instanceof File) {
+            const base64 = await getBase64(fileList.file);
+            setImageUrl(base64);
+            onBase64Change(base64);
         }
     };
 
     const uploadButton = (
         <S.Button type="button">
-            {loading ? <LoadingOutlined /> : <PlusOutlined />}
+            <PlusOutlined />
             <div>Выберите ваш аватар</div>
         </S.Button>
     );
