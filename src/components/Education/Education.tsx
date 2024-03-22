add7970c31c2498038436f1329767996e8568594
Valeriia <vvoshta@gmail.com>
2024-03-22 15:31:30 +0300

    update EducationSlice

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -4,20 +4,45 @@ import { Card } from '../Сard/Сard';
 import { FormItem } from '../FormItem/FormItem';
 import { DegreeOptions, degreeOptions } from './consts';
 
+import { useDispatch } from 'react-redux';
+import {
+    setInstitution,
+    setGraduationDate,
+    setDegree
+} from '../../store/reducers/EducationSlice';
+
 export const Education = () => {
+    const dispatch = useDispatch();
+
+    const handleInstitution = (e: React.ChangeEvent<HTMLInputElement>) => {
+        dispatch(setInstitution(e.target.value));
+    };
+
+    const handleGraduationDate = (dateString: string) => {
+        dispatch(setGraduationDate(dateString));
+    };
+
+    const handleDegree = (selectedDegree: DegreeOptions) => {
+        dispatch(setDegree(selectedDegree));
+    };
+
     return (
         <Card title="Образование">
             <FormItem label="Учреждение">
-                <Input placeholder="Учреждение" />
+                <Input placeholder="Учреждение" onChange={handleInstitution} />
             </FormItem>
             <FormItem label="Дата окончания">
-                <DatePicker placeholder="Дата окончания" />
+                <DatePicker
+                    placeholder="Дата окончания"
+                    onChange={handleGraduationDate}
+                />
             </FormItem>
             <FormItem label="Степень">
                 <Select
                     optionFilterProp="label"
                     defaultValue={DegreeOptions.Bachelor}
                     options={degreeOptions}
+                    onChange={handleDegree}
                 />
             </FormItem>
         </Card>
