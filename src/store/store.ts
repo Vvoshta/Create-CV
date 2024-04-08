d61949aeda28ac5eb0152a7590771c3aa08199dc
Valeriia <vvoshta@gmail.com>
2024-04-08 14:30:27 +0300

    update

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,18 +1,16 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import personalInfoReducer from './reducers/PersonalInfoSlice';
 import educationReducer from './reducers/EducationSlice';
 import skillsReducer from './reducers/SkillsSlice';
 import workExpReducer from './reducers/WorkExpSlice';
 
-const rootReducer = combineReducers({
-    personalInfoReducer,
-    educationReducer,
-    skillsReducer,
-    workExpReducer
-});
-
 export const store = configureStore({
-    reducer: rootReducer
+    reducer: {
+        personalInfoReducer,
+        educationReducer,
+        skillsReducer,
+        workExpReducer
+    }
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
